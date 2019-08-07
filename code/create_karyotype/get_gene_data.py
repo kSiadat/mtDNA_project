8e23ea402eb44cf7542922bf1222d7c58a966fd8
kSiadat <49532979+kSiadat@users.noreply.github.com>
2019-08-07 17:22:57 +0100

    exrtacted all data from .gff

--- code/create_karyotype/get_gene_data.py
+++ code/create_karyotype/get_gene_data.py
@@ -6,7 +6,11 @@ path = "../../data/"
 fileName = "gene_data_raw.txt"
 wget.download("https://www.ncbi.nlm.nih.gov/search/api/sequence/NC_012920", path+"mtDNA.fa")
 wget.download("https://www.ncbi.nlm.nih.gov/sviewer/viewer.cgi?db=nuccore&report=gff3&id=NC_012920.1", path+fileName)
-file = open(path+fileName)
 
-text = [line for line in file.readlines()]
-print(text)
+file = open(path+fileName)
+text = [line for line in file.readlines() if line[:11]=="NC_012920.1"]
+text = [line[19:-1] for line in text]
+text = [line.split("\t") for line in text]
+
+for line in text:
+    print(line)
