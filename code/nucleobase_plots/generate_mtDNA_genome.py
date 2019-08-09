a81ce54ee5075941c527de2c7c272a66a0276eb7
Conor Lawless <cnr.lwlss@gmail.com>
2019-08-09 16:47:52 +0100

    Some suggested edits

--- code/nucleobase_plots/generate_mtDNA_genome.py
+++ code/nucleobase_plots/generate_mtDNA_genome.py
@@ -1,15 +1,17 @@
-import random as rn
+import random
 
-path="..\..\data\mtDNA.fa"
+path="..\..\data\mtDNA.fa" # accession number instead?
+Nshuffle = 3
+outpaths = [f"../../data/mtDNA_random_{f+1}.fa" for f in range(Nshuffle)]
 
 file = open(path, "r")
 genome = list(file.read())
 file.close()
 
-for f in range(3):
-    rnGenome = genome[:]
-    rn.shuffle(rnGenome)
-    rnGenome = "".join(rnGenome)
-    file = open(f"../../data/mtDNA_random_{f+1}.fa", "w+")
-    file.write(rnGenome)
+rnGenome = genome[:]
+for f in range(Nshuffle):
+    random.shuffle(rnGenome)
+    rnGenstr = "".join(rnGenome)
+    file = open(outpaths[f], "w+") # accession numbe filename
+    file.write(rnGenstr)
     file.close()
