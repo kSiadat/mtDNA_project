ce14224aa8e8cd675c3ca35850b455a7781cc0ba
kSiadat <49532979+kSiadat@users.noreply.github.com>
2019-08-09 13:45:12 +0100

    subSeq is now imported

---
+++ code/nucleobase_plots/subSeq.py
@@ -0,0 +1,13 @@
+def subSeq(centre,window,seq):
+    '''Get a windowed subset of a circular string/sequence'''
+    start = centre - window
+    end = centre + window
+    if start >= 0 and end < len(seq):
+        subseq = seq[start:(end+1)]
+    elif start < 0 and end >= len(seq):
+        subseq = seq[start:]+seq+seq[0:(end-len(seq)+1)]
+    elif start < 0:
+        subseq = seq[start:]+seq[0:(end+1)]
+    elif end >= len(seq):
+        subseq = seq[start:]+seq[0:(end-len(seq)+1)]
+    return(subseq)
